1db63e9b38a59f7834bf1223c3b1b2769e73c5b9
NastiaPlaskonis <plaskonis.pn@ucu.edu.ua>
2023-02-08 23:31:03 +0200

    change

--- puzzle.py
+++ puzzle.py
@@ -1,9 +1,11 @@
 def check_columns(board: list):
     """
     checks whether there are no two or more identical digits in one column and returns True or False
-    >>> check_cols(["*1**4***5","**3***27*","4***2**6*","*76**91**", \
-    "**83**3**","*4**8**3*","***7*1**8","2**5***4*","*6***2**9"])
-    True
+    >>> check_columns(["**** ****",\
+"***1 **", "  3****", "* 4 1****",\
+"     9 5 ", " 6  83  *", "3   1  **",\
+"  8  2***", "  2  ****"])
+    False
     """
     for col in range(len(board[0])):
         curr_col_set = set()
